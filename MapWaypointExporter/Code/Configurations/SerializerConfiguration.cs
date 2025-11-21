a76d04c9097ee1f0adca5c81e5f1dbeca9fc0030
PaolaFalcon <5162247+PaolaFalcon@users.noreply.github.com>
2025-11-21 16:21:17 -0700

    Add map waypoint exporter service

---
+++ MapWaypointExporter/Code/Configurations/SerializerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MapWaypointExporter.Code.Configurations
+{
+    public static class SerializerConfiguration
+    {
+        public static readonly JsonSerializerOptions JsonSerializerDefaultOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        };
+    }
+}
--- MapWaypointExporter/MapWaypointExporterModSystem.cs
+++ MapWaypointExporter/MapWaypointExporterModSystem.cs
@@ -2,22 +2,15 @@
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
+using Vintagestory.GameContent;
 
 namespace MapWaypointExporter
 {
     public class MapWaypointExporterModSystem : ModSystem
     {
-
-        // Called on server and client
-        // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
             Mod.Logger.Notification("Hello from template mod: " + api.Side);
-        }
-
-        public override void StartServerSide(ICoreServerAPI api)
-        {
-            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("mapwaypointexporter:hello"));
         }
 
         public override void StartClientSide(ICoreClientAPI api)
@@ -25,5 +18,12 @@ namespace MapWaypointExporter
             Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("mapwaypointexporter:hello"));
         }
 
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("mapwaypointexporter:hello"));
+
+            var service = new Code.Services.MapWaypointExporterService(api);
+            service.Initialize();
+        }
     }
 }
