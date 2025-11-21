faa4d0df41b0c77a61d4d9fa3b58c68dd68045c7
Dagger <daggertlk@hotmail.com>
2025-11-21 14:57:48 -0700

    Add ColorExtensions

---
+++ MapWaypointExporter/assets/Extensions/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapWaypointExporter.assets.Extensions;
+
+public static class ColorExtensions
+{
+    public static string ToHexString(this int color)
+    {
+        uint unsignedArgb = unchecked((uint)color);
+        return $"#{unsignedArgb:X8}";
+    }
+}
